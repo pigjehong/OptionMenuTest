50781b3092db2954a6880ca0f1b185bd4e28446e
appcrate38 <appcrate38@appcrate38ui-iMac.local>
2017-05-23 19:57:03 +0900

    #4 OptionMenu Event Handling

--- app/src/main/java/com/example/appcrate38/optionmenutest/MainActivity.java
+++ app/src/main/java/com/example/appcrate38/optionmenutest/MainActivity.java
@@ -1,18 +1,25 @@
 package com.example.appcrate38.optionmenutest;
 
+import android.graphics.Color;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    LinearLayout linear;
+    Button but;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        linear=(LinearLayout) findViewById(R.id.linear);
+        but=(Button)findViewById(R.id.but_ok);
     }
 
     @Override
@@ -21,5 +28,28 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater mInflater=getMenuInflater();
         mInflater.inflate(R.menu.menu1, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch(item.getItemId()){
+            case R.id.item_blue:
+                linear.setBackgroundColor(Color.BLUE);
+                return true;
+            case R.id.item_red:
+                linear.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.item_yellow:
+                linear.setBackgroundColor(Color.YELLOW);
+                return true;
+            case R.id.item_but_rotate:
+                but.setRotation(but.getRotation()+45);
+                return true;
+            case R.id.item_but_size:
+                but.setScaleX( but.getScaleX()*1.5f);
+                return true;
+        }
+        return false;
     }
 }
